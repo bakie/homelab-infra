381b871b5ac7492c32f412d67ae385c543873a15
Bakie <bakelants.developer@gmail.com>
2024-11-23 20:36:59 +0100

    cleanup

--- initialize.tf
+++ initialize.tf
@@ -11,25 +11,3 @@ provider "libvirt" {
   uri   = "qemu+ssh://${var.kvm_construct01_user}@${var.kvm_construct01_hostname}/system?known_hosts_verify=ignore"
   alias = "construct01"
 }
-
-
-#provider "libvirt" {
-#  uri = "qemu+ssh://${var.kvm_user}@${var.kvm_hostname}/system"
-#}
-#terraform {
-#  required_providers {
-#    proxmox = {
-#      source  = "telmate/proxmox"
-#      version = "3.0.1-rc4"
-#    }
-#    vault = {
-#      source  = "hashicorp/vault"
-#      version = "4.3.0"
-#    }
-#  }
-#}
-#
-#provider "proxmox" {
-#  pm_api_url      = "https://${var.proxmox_host}:8006/api2/json"
-#  pm_tls_insecure = true
-#}
